006a9ebdc2008fc1665d12d27b8b28bf390a8705
luisf11 <luisfernandop1110@gmail.com>
2018-03-01 21:53:03 -0400

    Add item end point validation

--- server.js
+++ server.js
@@ -21,10 +21,16 @@ const getData = (item) => {
 
 app.post('/item', (req, res) => {
     let item = req.body.item;
+    if(!item){
+        res.statusCode= 400;
+        res.send({'message':'error item its empty or no present on the request',
+                    'status': '400'})
+        
+    }
     getData(item);
     setTimeout(() =>  res.send(data),1000 * 3);
 });
 
 
 app.listen(3031);
-// console.log("listening at port 3031");
+console.log("listening at port 3031");
