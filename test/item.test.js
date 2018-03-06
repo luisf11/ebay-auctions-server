8d5fc7a7b24f47bf902c4f41ba849f6b892b15ef
luisf11 <luisfernandop1110@gmail.com>
2018-03-05 22:11:47 -0400

    Add enpoint test

--- test/item.test.js
+++ test/item.test.js
@@ -1,6 +1,21 @@
+const request = require('supertest');
 const app = require('../server');
 
-test('hello test',()=> {
+describe('GET /',()=>{
+    test('it should response the GET method',async ()=> {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+    });
+})
 
-    
-});
+describe('POST /item',()=>{
+    test('it should response the POST method',async ()=> {
+        const response = await request(app).post('/item')
+        .set('Accept', 'application/json')
+        .send({item: 'macbook'})
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBeInstanceOf(Object);
+    });
+
+   
+})
